8eb3313f6634e17476a1a2b637831b9abc9a8503
Scott Miller <scott@millergeek.xyz>
2019-03-14 16:48:41 -0400

    init

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt
     
     echo -e -n (_pure_prompt_beginning)  # init prompt context (newline, etc.)
     echo -e (_pure_prompt_first_line)  # print current path, git branch/status, command duration
-    echo -e -n (_pure_prompt $exit_code)  # print prompt
+    echo -e -n (_pure_prompt_aws $exit_code)  # print prompt
     echo -e (_pure_prompt_ending)  # reset colors and end prompt
 
     set _pure_fresh_session false
